f6d06805cbcb91e16b3349e72d972ec3874572e7
cykic <cyb3rcykic@gmail.com>
2021-10-16 10:11:32 +0000

    initial

---
+++ script.sh
@@ -0,0 +1,18 @@
+echo -n 'username' | base64
+echo -n 'password' | base64
+
+# kubectl commands
+kubectl get secret
+kubectl get all
+kubectl get all | grep mongodb
+kubectl get pod | kubectl get pod -o wide
+kubectl get deployment
+kubectl get service
+kubectl delete deploy [NAME]
+kubectl describe pod [NAME]
+
+
+# Deployments
+kubectl apply -f mongo-secret.yaml
+kubectl apply -f mongo.yaml
+kubectl apply -f mongo-configmap.yaml 
